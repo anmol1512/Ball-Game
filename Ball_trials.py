cb1ca360ccaaf04000160a02aac627f03b20f3d5
Anmol Chhetri <anmol1512@gmail.com>
2021-04-09 22:58:31 +0530

    Successfully Drawn a Circle using draw.circle() in main loop

--- Ball_trials.py
+++ Ball_trials.py
@@ -4,8 +4,12 @@ width=height=700
 win=pygame.display.set_mode((width,height))
 pygame.display.set_caption('BALL GAME')
 run=True
+x=y=100
+radius=50
 while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+    pygame.draw.circle(win,(150,0,0),(x,y),50)
+    pygame.display.update()
 pygame.quit()
