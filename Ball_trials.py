b886b1c316dd3b38ec734fa464a4c533f06f7950
Anmol Chhetri <anmol1512@gmail.com>
2021-04-09 23:09:43 +0530

    Successfully made a moving ball using key.get_pressed()+win.fill()

--- Ball_trials.py
+++ Ball_trials.py
@@ -6,10 +6,21 @@ pygame.display.set_caption('BALL GAME')
 run=True
 x=y=100
 radius=50
+vel=1
 while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
-    pygame.draw.circle(win,(150,0,0),(x,y),50)
+    key=pygame.key.get_pressed()
+    if key[pygame.K_UP]:
+        y-=vel
+    if key[pygame.K_DOWN]:
+        y+=vel
+    if key[pygame.K_LEFT]:
+        x-=vel
+    if key[pygame.K_RIGHT]:
+        x+=vel
+    win.fill((0,0,0),rect=None)
+    pygame.draw.circle(win,(150,0,0),(x,y),radius)
     pygame.display.update()
 pygame.quit()
